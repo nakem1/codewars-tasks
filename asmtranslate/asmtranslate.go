2b7d0fe902ae0dcd6e4a4c6a63570c46622e2665
nakem1 <annaberdiev00@icloud.com>
2021-06-22 20:22:27 +0300

    assembly task

---
+++ asmtranslate/asmtranslate.go
@@ -0,0 +1,73 @@
+//This is the first part of this kata series. Second part is here.
+//
+//We want to create a simple interpreter of assembler which will support
+//the following instructions:
+//
+//mov x y - copies y (either a constant value or the content of a register) into register x
+//inc x - increases the content of the register x by one
+//dec x - decreases the content of the register x by one
+//jnz x y - jumps to an instruction y steps away (positive means forward,
+//negative means backward, y can be a register or a constant), but only if
+//x (a constant or a register) is not zero
+//Register names are alphabetical (letters only). Constants are always
+//integers (positive or negative).
+//
+//Note: the jnz instruction moves relative to itself. For example, an offset
+//of -1 would continue at the previous instruction, while an offset of 2 would skip over the next instruction.
+//
+//The function will take an input list with the sequence of the program
+//instructions and will execute them. The program ends when there are no more
+//instructions to execute, then it returns a dictionary with the contents of the registers.
+//
+//Also, every inc/dec/jnz on a register will always be preceeded by a mov on
+//the register first, so you don't need to worry about uninitialized registers.
+package asmtranslate
+
+import (
+	"strconv"
+	"strings"
+)
+
+type asm map[string]int
+
+func (asm asm) mov(dest string, src string) {
+	if val, err := strconv.Atoi(src); err != nil {
+		asm[dest] = asm[src]
+	} else {
+		asm[dest] = val
+	}
+}
+
+func (asm asm) inc(x string) {
+	asm[x] += 1
+}
+
+func (asm asm) dec(x string) {
+	asm[x] -= 1
+}
+
+func handleCommand(registers asm, split []string, val string) {
+	switch split[0] {
+	case "mov":
+		registers.mov(split[1], split[2])
+	case "inc":
+		registers.inc(split[1])
+	case "dec":
+		registers.dec(split[1])
+	}
+}
+
+func SimpleAssembler(program []string) map[string]int {
+	registers := make(asm)
+	size := len(program)
+	for i := 0; i < size; i++ {
+		split := strings.Fields(program[i])
+		if split[0] == "jnz" && registers[split[1]] != 0 {
+			step, _ := strconv.Atoi(split[2])
+			i += step
+			split = strings.Fields(program[i])
+		}
+		handleCommand(registers, split, program[i])
+	}
+	return (registers)
+}
--- main/main.go
+++ main/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 
-	"github.com/nakem1/codewars_tasks/validbraces"
+	"github.com/nakem1/codewars_tasks/asmtranslate"
 )
 
 func main() {
 	// val := os.Args[1]
 	// val1, err := strconv.Atoi(os.Args[2])
 	// if err != nil {
-		// panic(err)
+	// panic(err)
 	// }
-	fmt.Printf("%v\n", validbraces.ValidBraces("(({{[[]]}}))"))
+	str := []string{"mov a 1", "mov b 1", "mov c 0", "mov d 26", "jnz c 2",
+		"jnz 1 5", "mov c 7", "inc d", "dec c", "jnz c -2", "mov c a", "inc a",
+		"dec b", "jnz b -2", "mov b c", "dec d", "jnz d -6", "mov c 18",
+		"mov d 11", "inc a", "dec d", "jnz d -2", "dec c", "jnz c -5"}
+	fmt.Printf("%#v\n", asmtranslate.SimpleAssembler(str))
 }
